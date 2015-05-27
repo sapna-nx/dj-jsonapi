aed360220ad409a0110bb247b0ba0b2a5c5d64b1
Ryan P Kilby <rpkilby@ncsu.edu>
2015-05-26 23:57:44 -0400

    Updated serializer docs

--- json_api/serializers.py
+++ json_api/serializers.py
@@ -4,8 +4,11 @@ from rest_framework import serializers
 
 class ResourceSerializer(serializers.ModelSerializer):
     """
-    A type of `ModelSerializer` that represents individual resources. It is
-    agnostic to its identity and its relationships to other resources.
+    A type of `ModelSerializer` that represents a resource object's attributes.
+    It is agnostic to its identity and its relationships to other resources.
+
+    Reference:
+    http://jsonapi.org/format/#document-structure-resource-objects
     """
 
     def get_default_field_names(self, declared_fields, model_info):
