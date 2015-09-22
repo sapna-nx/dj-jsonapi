ee737f927db73ce6a29714be4aff7ef1962b4163
Ryan P Kilby <rpkilby@ncsu.edu>
2015-09-22 19:51:30 -0400

    Add querystring check to unquote_brackets

--- json_api/utils/urls.py
+++ json_api/utils/urls.py
@@ -1,5 +1,8 @@
 
 def unquote_brackets(url):
+    if '?' not in url:
+        return url
+
     url, query = url.split('?', 1)
     query = query.replace('%5B', '[')
     query = query.replace('%5D', ']')
